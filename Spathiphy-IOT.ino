357483dad7a7f74a9ed6f981d621d17b4e7a4a12
Iván L. Murillo <diskman199@gmail.com>
2021-10-06 21:41:17 -0500

    Lectura de sensor de temperatura y humedad

--- Spathiphy-IOT.ino
+++ Spathiphy-IOT.ino
@@ -1,12 +1,20 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+#include "DHTStable.h"
+
+DHTStable DHT;
+
+#define DHT22_PIN D0
+
+void setup()
+{
   Serial.begin(9600);
+  Serial.println("Sensors starting");
 }
 
-void loop() {
-  Serial.print("Hola mundo\n");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+void loop()
+{
+  DHT.read22(DHT22_PIN);
+  float hum = DHT.getHumidity();
+  float temp = DHT.getTemperature();
+  Serial.println("Temperatura: " + String(temp) + "\tHumedad: " + String(hum) + "%");
+  delay(2000);
 }
