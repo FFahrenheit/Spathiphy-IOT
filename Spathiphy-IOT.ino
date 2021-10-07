d1344fe23732a999273478ab8e76178791d9cfe2
Iván L. Murillo <diskman199@gmail.com>
2021-10-06 22:00:00 -0500

    Lectura de luz

--- Spathiphy-IOT.ino
+++ Spathiphy-IOT.ino
@@ -1,13 +1,26 @@
 #include "DHTStable.h"
-
-DHTStable DHT;
+#include <Wire.h>
+#include <BH1750.h>
 
 #define DHT22_PIN D0
 
+DHTStable DHT;
+BH1750 LUX(0x23);
+
 void setup()
 {
+  Wire.begin(D2, D1);
   Serial.begin(9600);
   Serial.println("Sensors starting");
+  if (LUX.begin()) 
+  {
+    Serial.println("BH1750 initialised");
+  }
+  else
+  {
+    Serial.println("Error initialising BH1750");
+  }
+
 }
 
 void loop()
@@ -15,6 +28,7 @@ void loop()
   DHT.read22(DHT22_PIN);
   float hum = DHT.getHumidity();
   float temp = DHT.getTemperature();
-  Serial.println("Temperatura: " + String(temp) + "\tHumedad: " + String(hum) + "%");
+  float light = LUX.readLightLevel();//Realizamos una lectura del sensor
+  Serial.println("Temperatura: " + String(temp) + "\tHumedad: " + String(hum) + "%\t\tLuminosidad: " + String(light));  
   delay(2000);
 }
