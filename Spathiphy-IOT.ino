c341053754b32a19edf8f9b1e802c25df17524d2
Iván L. Murillo <diskman199@gmail.com>
2021-10-22 18:57:38 -0500

    Conexión a una red WiFi

--- Spathiphy-IOT.ino
+++ Spathiphy-IOT.ino
@@ -1,8 +1,12 @@
+#include <ESP8266WiFi.h>
 #include "DHTStable.h"
 #include <Wire.h>
 #include <BH1750.h>
 
 #define DHT22_PIN D0
+
+const char * ssid = "TheCoolestWiFiLM";
+const char * password = "LopezMurillo128";
 
 DHTStable DHT;
 BH1750 LUX(0x1D);
@@ -25,6 +29,20 @@ void setup()
   }
 
   Serial.println("BH1750 initialised");
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+  int i = 0;
+    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP()); 
 }
 
 void loop()
